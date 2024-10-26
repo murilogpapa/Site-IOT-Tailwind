92160cf69f2cf3fd092cd877385d294e02482ecc
Muc <mucteste@gmail.com>
2024-10-25 22:03:18 -0300

    faixa (painel inicial) do site pronta, e primeiros paineis iniciados.

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function Menu(e){
-    let list = document.querySelector('ul');
-    
-    e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
+function Menu(e) {
+    let list = document.querySelector('div.absolute');
+
+    if (e.classList.contains('fa-bars')) {
+        e.classList.remove('fa-bars');
+        e.classList.add('fa-xmark');
+        list.classList.add('h-[235px]');
+        list.classList.add('opacity-80');
+        list.classList.add('backdrop-opacity-100');
+
+    } else {
+        e.classList.remove('fa-xmark');
+        e.classList.add('fa-bars');
+        list.classList.remove('h-[235px]');
+        list.classList.remove('opacity-80');
+        // list.classList.remove('backdrop-opacity-100');
+    }
 }
