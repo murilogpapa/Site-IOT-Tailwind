9e966e59b6c7998922939caad25cea543eaa274b
Muc <mucteste@gmail.com>
2024-11-04 08:47:23 -0300

    problema do dropmenu resolvido e pagina de areas do conhecimento recomeçada

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ function Menu(e) {
         list.classList.add('h-[235px]');
         list.classList.add('opacity-80');
         list.classList.add('backdrop-opacity-100');
-
+        list.classList.remove('mt-[1000rem]'); // Remove a margem alta quando aparece
+        list.classList.add('mt-[0.1rem]'); // Adiciona a margem padrão quando aparece
     } else {
         e.classList.remove('fa-xmark');
         e.classList.add('fa-bars');
         list.classList.remove('h-[235px]');
         list.classList.remove('opacity-80');
         // list.classList.remove('backdrop-opacity-100');
+        list.classList.remove('mt-[0.1rem]'); // Remove a margem padrão quando some
+        list.classList.add('mt-[1000rem]'); // Adiciona a margem alta quando some
     }
 }
